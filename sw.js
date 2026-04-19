d053456a5f5a4b0aad38e083ff2313aed6c46296
CAPBARAER <capillasbarajas@outlook.com>
2026-04-19 18:28:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,16 @@
+// Service Worker de limpieza
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+// La lógica de push se queda comentada o vacía para que no dé errores
+/*
 self.addEventListener('push', function(e) {
   const data = e.data.json();
   self.registration.showNotification("Misa Aeropuerto", { body: data.msg });
 });
+*/
